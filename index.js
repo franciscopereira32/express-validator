cc125faab6cd5ef4cd4bad589e1afa712dd2a82a
Francisco Pereira <80694995+franciscopereira32@users.noreply.github.com>
2024-05-01 19:21:46 +0930

    Accessing validated data

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { error } = require('console');
 const express = require('express');
-const { query, validationResult } =  require('express-validator');
+const { query, matchedData, validationResult } =  require('express-validator');
 const app = express();
 
 app.use(express.json());
 app.get('/hello', query('person').notEmpty().escape(), (req, res) => {
   const result = validationResult(req);
   if (result.isEmpty()) {
+    const data = matchedData(req);
     return res.send(`Hello, ${req.query.person}!`);
   }
 
